7d81b9984afc2c14097780a177ee2c14de0b5081
Le Cong Tuan <congtuanle23@gmail.com>
2021-06-01 04:12:42 +0200

    style product item display

--- public/js/product.js
+++ public/js/product.js
@@ -6,13 +6,27 @@ function product__click(event) {
 }
 
 const current_page = window.location.href.split('/').slice(-1)[0];
-const numpage = document.querySelector('#numpage').value;
+const numpage_item = document.querySelector('#numpage');
+const prev_link = document.querySelector('#prev_link');
+const next_link = document.querySelector('#next_link');
+var numpage = null;
+if (numpage_item){
+    numpage = numpage_item.value;
+}
 
 if (Number(current_page) < 1 || Number(current_page) > Number(numpage)) {
     window.location.href = '/products/page/1';
 }
 
-const prev_link = document.querySelector('#prev_link');
+if (Number(current_page) ===1) {
+    prev_link.hidden = true;
+
+}
+if (Number(current_page) === Number(numpage)) {
+    next_link.hidden = true;
+}
+
+
 if (prev_link){
     prev_link.addEventListener('click', () => {
         if (Number(current_page) > 1) {
@@ -22,7 +36,7 @@ if (prev_link){
     })
 }
 
-const next_link = document.querySelector('#next_link');
+
 if (next_link){
     next_link.addEventListener('click', () => {
         if (Number(current_page) < Number(numpage)){
