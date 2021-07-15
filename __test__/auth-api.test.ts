018d9163b708215c8754fde6a1d6f569bd28ef7d
Le Cong Tuan <congtuanle23@gmail.com>
2021-07-16 01:35:16 +0200

    update auth test

--- __test__/auth-api.test.ts
+++ __test__/auth-api.test.ts
@@ -17,14 +17,14 @@ test("POST /api/auth/signup", async () => {
     expect(res.statusCode).toBe(201);
 });
 
-let cookie:string;
+let cookie: string;
 
 test("POST /api/auth/login", async () => {
     const res = await request(server)
         .post("/api/auth/login")
         .send({ username: "jtest", password: "azerty" });
     expect(res.headers["set-cookie"]).toBeTruthy();
-    cookie = (res.headers["set-cookie"]);
+    cookie = res.headers["set-cookie"];
     expect(res.statusCode).toBe(200);
 });
 
@@ -32,6 +32,15 @@ test("POST /api/auth/logout", async () => {
     const res = await request(server)
         .post("/api/auth/logout")
         .set("Cookie", [cookie]);
-    expect(res.headers["set-cookie"]).toBeTruthy();
     expect(res.statusCode).toBe(200);
-})
+});
+
+test("GET /api/auth/info", async () => {
+    const res = await request(server)
+        .get("/api/auth/info")
+        .set("Cookie", [cookie]);
+    expect(res.statusCode).toBe(200);
+    expect(res.body.uid).toBeTruthy();
+    expect(res.body.token).toBeTruthy();
+    expect(res.body.role).toEqual("NORMAL_USER");
+});
